5c3de3e5f5ce095061f19d505a4f49ca09a49e18
sandrm <sandrm@ukr.net>
2025-02-19 00:37:48 +0200

    rebased (fork) feature & main (base)

--- demo-proj/src/main/java/org/example/App.java
+++ demo-proj/src/main/java/org/example/App.java
@@ -12,6 +12,8 @@ public class App {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15, 16, 17,18 ,19,20};
         Arrays.stream(arr).forEach(n -> System.out.println(n));
 
+        System.out.println("Sandrm user changes");
+
         System.out.println("App finished!");
     }
 }
